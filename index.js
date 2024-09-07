a3590cd1814a8e8ee135127f51a9ddad3a5b4209
hatimlechgar <h.lechgar@gmail.com>
2024-09-07 05:27:16 +0200

    change missions code

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ app.use(express.json()); // Parse JSON request body
 
 // Define missions
 const missions = [
-  { id: 1, name: 'Data Analysis Project', requiredSkills: ['Python', 'SQL', 'Excel', 'Statistics', 'Data Modeling'] },
-  { id: 2, name: 'Web Development', requiredSkills: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'React'] },
-  { id: 3, name: 'Cloud Migration', requiredSkills: ['AWS', 'Azure', 'Docker', 'Kubernetes', 'Linux'] },
-  { id: 4, name: 'Mobile App Development', requiredSkills: ['Java', 'Kotlin', 'Android', 'iOS', 'Swift'] },
-  { id: 5, name: 'DevOps Pipeline Setup', requiredSkills: ['Jenkins', 'Docker', 'Git', 'CI/CD', 'Linux'] },
-  { id: 6, name: 'Machine Learning Project', requiredSkills: ['Python', 'TensorFlow', 'Pandas', 'Deep Learning', 'Statistics'] },
-  { id: 7, name: 'Database Optimization', requiredSkills: ['SQL', 'NoSQL', 'PostgreSQL', 'MongoDB', 'Database Design'] },
-  { id: 8, name: 'Cybersecurity Audit', requiredSkills: ['Penetration Testing', 'Firewalls', 'Security Auditing', 'Linux', 'Networking'] },
-  { id: 9, name: 'Game Development', requiredSkills: ['Unity', 'C#', '3D Modeling', 'Animation', 'Game Design'] },
-  { id: 10, name: 'eCommerce Platform', requiredSkills: ['Shopify', 'JavaScript', 'React', 'Node.js', 'Payment Gateways'] },
+  { id: 1, name: 'Data Analysis Project for AXA-45AZ', requiredSkills: ['Python', 'SQL', 'Excel', 'Statistics', 'Data Modeling'] },
+  { id: 2, name: 'Web Development for Google-12JK', requiredSkills: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'React'] },
+  { id: 3, name: 'Cloud Migration for Microsoft-09YU', requiredSkills: ['AWS', 'Azure', 'Docker', 'Kubernetes', 'Linux'] },
+  { id: 4, name: 'Mobile App Development for Facebook-87OP', requiredSkills: ['Java', 'Kotlin', 'Android', 'iOS', 'Swift'] },
+  { id: 5, name: 'DevOps Pipeline Setup for Amazon-22WE', requiredSkills: ['Jenkins', 'Docker', 'Git', 'CI/CD', 'Linux'] },
+  { id: 6, name: 'Machine Learning Project for IBM-34PL', requiredSkills: ['Python', 'TensorFlow', 'Pandas', 'Deep Learning', 'Statistics'] },
+  { id: 7, name: 'Database Optimization for Oracle-89IU', requiredSkills: ['SQL', 'NoSQL', 'PostgreSQL', 'MongoDB', 'Database Design'] },
+  { id: 8, name: 'Cybersecurity Audit for Cisco-77QM', requiredSkills: ['Penetration Testing', 'Firewalls', 'Security Auditing', 'Linux', 'Networking'] },
+  { id: 9, name: 'Game Development for Ubisoft-16TR', requiredSkills: ['Unity', 'C#', '3D Modeling', 'Animation', 'Game Design'] },
+  { id: 10, name: 'eCommerce Platform for Shopify-56ZX', requiredSkills: ['Shopify', 'JavaScript', 'React', 'Node.js', 'Payment Gateways'] }
 ];
+
 
 // POST route to suggest missions based on skills
 app.post('/suggest-missions', (req, res) => {
